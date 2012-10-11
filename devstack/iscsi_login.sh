fe8f485fde931f863469422ad4ea54a3ba20e6ef
liyang <zz7a5pe4@gmail.com>
2012-10-12 07:35:17 +0800

    check status before login iscsi, by vc

--- devstack/iscsi_login.sh
+++ devstack/iscsi_login.sh
@@ -1,15 +1,32 @@
 #!/bin/bash
-#use HOST_IP as iScsi target server Ip
 
 if [[ $TOP_DIR -eq "" ]]; then
     TOP_DIR="/home/xeven/devstack"
 fi
+
 source $TOP_DIR/localrc
+source $TOP_DIR/openrc admin admin
+
+#use HOST_IP as iScsi target server Ip
+function login_inuse_volume {
+    for volID in `nova volume-list |grep in-use|cut -d' ' -f2`
+    do
+        if [ "$volID" = "$1" ]; then
+            printf "Logging $2 ......\n"
+            sudo iscsiadm -m node -T $2 -p $HOST_IP --login
+        fi
+    done
+}
+
+
 
 for iqn in `sudo iscsiadm -m discovery -t sendtargets -p $HOST_IP|cut -d' ' -f2`
 do
-    printf "Logging $iqn ......\n"
-    sudo iscsiadm -m node -T $iqn -p $HOST_IP --login
+#   get id and transfer from hex id to dec id 
+    id_hex=0x${iqn:0-8}
+    volumeid=$((id_hex))
+    login_inuse_volume $volumeid $iqn
 done
 
 echo "done"
+
